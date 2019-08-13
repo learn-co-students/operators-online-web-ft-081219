c27fb8a009afe2054b0446e90ffa8d7e4753a994
Caleb Kavanaugh <calebck@hotmail.com>
2019-08-13 20:04:35 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,11 +1,19 @@
-def unsafe?(speed)
+#returns true if speed is less than 40 or greater than 60
 
+def unsafe?(speed)
+  answer = speed
+
+  if answer > 40 && answer < 60
+    return false
+  else
+    return true
+  end 
 end
 
-
-
 def not_safe?(speed)
-	
+	answer = speed
+  
+  answer > 40 && answer < 60 ? false : true
 end
 	
 
